d52f09ad57b63d7bb8f48f951112b86dd0bf2be5
Walter Taño <tano.walter@hotmail.com>
2022-09-26 20:15:34 -0300

    initial config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { LoginFormComponent } from './components/login-page/login-form/login-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'test', component: LoginFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
