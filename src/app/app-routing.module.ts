a6d7709f8317ce2bffb7231d3a7936c85ddf498e
RafaFil <filardisafa@gmail.com>
2022-09-28 00:54:06 -0300

    init on the way

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
-import { LoginFormComponent } from './components/login-page/login-form/login-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'test', component: LoginFormComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginFormComponent } from './components/login-page/login-form/login-form.component';
+import { LogoImageComponent } from './components/general/logo-image/logo-image.component';
+import { InitHomeComponent } from './components/init-home/init-home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LogoImageComponent,
+    InitHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
