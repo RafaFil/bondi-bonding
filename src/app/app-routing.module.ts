496983bb88f2026de7316477f5092ade45ec39b8
Walter Taño <tano.walter@hotmail.com>
2022-09-28 18:41:52 -0300

    started working on login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'test', component: LoginFormComponent }
+  { path: '', component: LoginFormComponent }
 ];
 
 @NgModule({
